492b520d762f2e91706527643930d12884affca6
emozsaurab <emozsaurab@live.in>
2018-01-04 13:14:26 +0530

    message from emosrb

--- src/com/gittutorial/GitTutorial.java
+++ src/com/gittutorial/GitTutorial.java
@@ -1,6 +1,9 @@
 package com.gittutorial;
 
 public class GitTutorial {
+    public void messageFromEmosrb(){
+     System.out.println("from emosrb");   
+    }
 public static void main(String[] args) {
     
 }
